beeb903e89d5f805736a2e261f8dbd1dde0d585c
Aayush <k.aayush.1509@gmail.com>
2024-09-14 01:26:15 +0530

    bo_live_v4.2

--- app/AppData/http/booking.js
+++ app/AppData/http/booking.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { API_ENV } from './provider.js'
 const local = "http://localhost:3300/devstag/v1/api";
 const aws = "https://rrucc6gtnvp7ldakroqqedhcae0hguqh.lambda-url.ap-south-1.on.aws/devstag/v1/api";
 
 const api = axios.create({
-    baseURL: aws,
+    baseURL: API_ENV === "PROD" ? aws : local,
     withCredentials: true,
     headers: {
         'Content-type': 'application/json',
